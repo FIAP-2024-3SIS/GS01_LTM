a95d95a4c15cadc9a218498184ed3d26201aee75
murilojesusgr <159389267+murilojesusgr@users.noreply.github.com>
2024-06-05 21:40:07 -0300

    Update condicoes-oceanicas.service.ts

--- src/app/condicoes-oceanicas.service.ts
+++ src/app/condicoes-oceanicas.service.ts
@@ -1,1 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
+@Injectable({
+  providedIn: 'root'
+})
+export class CondicoesOceanicasService {
+  private apiUrl = 'https://fiap-3sis-gs-20241.azurewebsites.net';
+
+  constructor(private http: HttpClient) {}
+
+  getCondicoesOceanicas(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+}
